357ac4c6b7b20a878b4f531de6b36f6869ade4a3
Niklas Grebe <niklas.grebe@bytro.com>
2022-08-23 17:01:53 +0200

    Perf2 with updated deps

--- db/init-db.sh
+++ db/init-db.sh
@@ -5,7 +5,7 @@ sleep 30
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 
-CREATE SCHEMA library 
+CREATE SCHEMA library
 AUTHORIZATION postgres;
 
 CREATE SEQUENCE library.books_book_id_seq
@@ -14,7 +14,7 @@ CREATE SEQUENCE library.books_book_id_seq
     MINVALUE 1
     MAXVALUE 2147483647
     CACHE 1;
-    
+
 CREATE SEQUENCE library.orders_order_id_seq
     INCREMENT 1
     START 1
@@ -28,22 +28,22 @@ CREATE SEQUENCE library.users_user_id_seq
     MINVALUE 1
     MAXVALUE 2147483647
     CACHE 1;
-   
+
 CREATE TABLE library.books
 (
     book_id integer NOT NULL DEFAULT nextval('library.books_book_id_seq'::regclass),
-    title text COLLATE pg_catalog."default" NOT NULL,
-    author text COLLATE pg_catalog."default" NOT NULL,
-    isbn text COLLATE pg_catalog."default" NOT NULL,
-    year integer NOT NULL,
+    title text COLLATE pg_catalog."default" NULL,
+    author text COLLATE pg_catalog."default" NULL,
+    isbn text COLLATE pg_catalog."default" NULL,
+    year integer NULL,
     CONSTRAINT books_pkey PRIMARY KEY (book_id)
 );
 
 CREATE TABLE library.users
 (
     user_id integer NOT NULL DEFAULT nextval('library.users_user_id_seq'::regclass),
-    first_name text COLLATE pg_catalog."default" NOT NULL,
-    last_name text COLLATE pg_catalog."default" NOT NULL,
+    first_name text COLLATE pg_catalog."default" NULL,
+    last_name text COLLATE pg_catalog."default" NULL,
     CONSTRAINT users_pkey PRIMARY KEY (user_id)
 );
 
@@ -64,5 +64,5 @@ CREATE TABLE library.orders
         ON DELETE NO ACTION
 );
 
-	
+
 EOSQL
